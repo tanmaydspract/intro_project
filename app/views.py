566b717dbc6ca69ee62013937b5495bbd3afef0a
tanmaydspract <dixit.tanmay22@gmail.com>
2025-07-06 16:43:44 +0530

    feat: workflow updated

--- app/views.py
+++ app/views.py
@@ -7,6 +7,8 @@ from rest_framework.response import Response
 class IntroClass(APIView):
 
     def get(self, request):
+        if not request.query_params.get('name'):
+            return Response({'msg':'please provied name'})
         if not request.query_params.get('a1') or not request.query_params.get('a2'):
             return Response({"msg":f"hello {request.query_params.get('name')}!! this is from production, you have not provided calculation data"})    
         return Response({"msg":f"hello {request.query_params.get('name')}!! this is from production, your calculation : {str(int(request.query_params.get('a1'))+int(request.query_params.get('a2')))}"})
