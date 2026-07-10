e44c29d0d70ac77d61fed41c385d8a83135c110f
Tanmay Dixit <dixit.tanmay22@gmail.com>
2026-07-10 23:08:35 +0530

    minor changes in api response

--- app/views.py
+++ app/views.py
@@ -11,4 +11,4 @@ class IntroClass(APIView):
             return Response({'msg':'please provied name'})
         if not request.query_params.get('a1') or not request.query_params.get('a2'):
             return Response({"msg":f"hello {request.query_params.get('name')}!! this is from production, you have not provided calculation data"})    
-        return Response({"msg":f"hello {request.query_params.get('name')}!! this is from production, your calculation : {str(int(request.query_params.get('a1'))+int(request.query_params.get('a2')))}"})
+        return Response({"msg":f"hello {request.query_params.get('name')}!! this is from latest production enviornment, your calculation : {str(int(request.query_params.get('a1'))+int(request.query_params.get('a2')))}"})
